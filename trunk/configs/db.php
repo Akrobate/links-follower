8ba81c0bff1b8bd4de4285ba9b4bd68447330cef
fedorov.artiom@gmail.com <fedorov.artiom@gmail.com@800ae1e3-4bcb-b34a-313c-bb881cc8a960>
2012-05-21 08:58:20 +0000

    rentrés dans la copie de travail par erreur. Doivent rester des fichiers statiques

--- trunk/configs/db.php
+++
@@ -1,9 +0,0 @@
-<?
-
-	define('DB_HOST', "127.0.0.1"); // replace ... by your host
-	define('DB_USER', "root"); // replace ... by your user
-	define('DB_PASSWORD', "kohanski00380"); // replace ... by your user
-	define('DB_NAME', "follower");
-	
-	
-?>
